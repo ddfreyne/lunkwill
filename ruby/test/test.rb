8a9c126101ef627760c4fcd405ced1ca913134f5
Denis Defreyne <denis.defreyne@stoneship.org>
2009-01-10 13:27:38 +0100

    removing ruby version

--- ruby/test/test.rb
+++
@@ -1,8 +0,0 @@
-# Add to load path
-$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + '/../lib'))
-
-# Load lunkwill
-require 'lunkwill'
-
-# Test
-arg = Lunkwill::LWArgumentCreateFromString('Blah')
